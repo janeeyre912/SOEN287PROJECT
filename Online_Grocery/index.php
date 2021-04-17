96276eb490af23124df03712e85d3a7118072a2f
KabyDaKabenz <76658191+KabyDaKabenz@users.noreply.github.com>
2021-04-17 22:57:10 +0400

    Update index.php

--- Online_Grocery/index.php
+++ Online_Grocery/index.php
@@ -16,31 +16,31 @@
 
       <div class="row">
         <div class="col-sm">
-          <a href="../Online_Grocery/Fruits&Vegetables/fruits_vegetables.html">
+          <a onclick="window.location.href='basicPage.php?fruits_vegetables'">
             <img class="homeImg2 img-fluid animated fadeInUp" src="../Online_Grocery/img/fruitnVeg.jpg" alt="">
         </div>
         <div class="col-sm">
-          <a href="../Online_Grocery/dairy_eggs/dairy_eggs.html">
+          <a onclick="window.location.href='basicPage.php?dairy_eggs'">
             <img class="homeImg2 img-fluid animated fadeInUp" src="../Online_Grocery/img/dairy.jpg" alt="" href="">
         </div>
         <div class="col-sm">
-          <a href="../Online_Grocery/seaFood/seafood.html">
+          <a onclick="window.location.href='basicPage.php?seafood'">
             <img class="homeImg2 img-fluid animated fadeInUp" src="../Online_Grocery/img/seafood.jpg" alt="">
         </div>
       </div>
       <br>
       <div class="row">
         <div class="col-sm">
-          <a href="../Online_Grocery/Meat&Poultry/MeatAndPoultry.html">
+          <a onclick="window.location.href='basicPage.php?meat_poultry'">
             <img class="homeImg2 img-fluid animated fadeInUp" src="../Online_Grocery/img/meat.png" alt="">
         </div>
         <div class="col-sm">
-          <a href="../Online_Grocery/Beer&Wine/beer_wine.html">
+          <a onclick="window.location.href='basicPage.php?beer_wine'">
             <img class="homeImg2 img-fluid animated fadeInUp" src="../Online_Grocery/img/beerWine.png" alt="">
 
         </div>
         <div class="col-sm">
-          <a href="../Online_Grocery/Beverages/">
+          <a onclick="window.location.href='basicPage.php?beverages'">
             <img class="homeImg2 img-fluid animated fadeInUp" src="../Online_Grocery/img/beverage.png" alt="">
         </div>
       </div>
