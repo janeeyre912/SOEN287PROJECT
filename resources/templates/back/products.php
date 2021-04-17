f61f9a89a06f98f7c9e278f3b387a24d176c5778
KabyDaKabenz <76658191+KabyDaKabenz@users.noreply.github.com>
2021-04-17 23:03:48 +0400

    Add files via upload

---
+++ resources/templates/back/products.php
@@ -0,0 +1,35 @@
+<div class="main col-lg-9 col-md-9 py-3 flex-grow-1 ">
+        <!--users information-->
+            <!--    breadcrumb link-->
+            <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index_bs.html">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Products</li>
+                </ol>
+            </nav>
+            <table class="table table-hover" role="table">
+                <thead role="rowgroup">
+                <tr role="row">
+                    <th scope="col" role="columnheader">Item number</th>
+                    <th scope="col" role="columnheader">Product</th>
+                    <th scope="col" role="columnheader">Aisle</th>
+                    <th scope="col" role="columnheader">Price</th>
+                    <th scope="col" role="columnheader">Quantity in stock</th>
+                    <th scope="col" role="columnheader"></th>
+                </tr>
+                </thead>
+                <tbody role="rowgroup">
+                  
+                <?php displayProductList(); ?>
+              
+                </tbody>
+            </table>
+            <div class="row justify-content-end">
+            <div>
+                <button type="button" class="btn btn-dark"onclick="window.location.href='index.php?add_product.php'">Add a product</button>
+                <button type="button" class="btn btn-dark btn-confirm2">Save</button>
+                <button type="button" class="btn btn-dark btn-confirm1"; style="margin-right: 20px;">Cancel</button>
+            </div>
+</div>
+</div>
+
