5a8f7bdbc6c64c09c2ca2baa7a27ed852ff8cdcb
janeeyre912 <57924747+janeeyre912@users.noreply.github.com>
2021-04-19 12:00:08 -0400

    Update header.php

--- resources/templates/back/header.php
+++ resources/templates/back/header.php
@@ -6,7 +6,6 @@
     <title>Back store home page</title>
     <!--  CSS Stylesheets-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/back_store.css">
     <link rel="stylesheet" href="css/user_info.css">
     <link rel="stylesheet" href="css/index_bs.css">
 
