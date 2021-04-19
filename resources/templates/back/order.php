e3dce99bc6bc8dcca6dabed6733ae3df6f7362e0
WuyangP <pengwuyang2years@gmail.com>
2021-04-19 12:16:33 -0400

    Update order.php

--- resources/templates/back/order.php
+++ resources/templates/back/order.php
@@ -1,3 +1,5 @@
+
+
 <?php
  
 ?>
@@ -14,21 +16,24 @@
         request.onload = () => {
             //Parse the returned JSON array.
             let orders = JSON.parse(request.response);
-
+            var orderlist = document.querySelector(".orderlist");
             if(orders.length == 0) {
-                document.getElementById('orders').innerHTML += `There are currently no orders.`;
+                orderlist.innerHTML += `There are currently no orders.`;
             }
             else {
                 console.log(orders);
-                orders.forEach(order => {
-                document.getElementById('orders').innerHTML +=
-                    `<tr><td>${order.user}</td>
-                    <td>${order.date}</td>
-                    <td>#${order.id}</td>
+                orders.forEach(orders => {
+                orderlist.innerHTML +=
+                    `<tr role="row">
+                    <td row="cell">${orders.orderid}</td>
+                    <td row="cell">${orders.orderdate}</td>
+                    <td row="cell">${orders.orderby}</td>
+                    <td row="cell">${orders.email}</td>
                     <td>
-                    <button type="button" class="edit-btn" onclick="window.location.href='EditOrderProfile.html'">Edit Order</button>
-                    <button type="button" class="cancel-btn">Delete Order</button>
-                    </td></tr>`;
+                    <button type="button" class="btn btn-sm btn-dark" onclick ="window.location.href = 'index.php?edit_user&id={$user->id}'">Edit</button>
+                    <button type="button" class="btn btn-sm btn-danger" onclick ="window.location.href = 'index.php?delete_user_id={$user->id}'">Delete</button>
+                    </td> 
+                    </tr> `;
                 });
             }
         };
@@ -39,33 +44,32 @@
 
 </script>
 
-<!-- Content -->
 
-    <div class="main col-lg-9 col-md-9 py-3 flex-grow-1 ">
+<div class="main col-lg-9 col-md-9 py-3 flex-grow-1 ">
         <!--users information-->
             <!--    breadcrumb link-->
             <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Order List</li>
+                    <li class="breadcrumb-item active" aria-current="page">users</li>
                 </ol>
             </nav>
-<div class="center-area">
+            <button type="button" class="btn btn-dark btn-adduser"onclick="window.location.href='index.php?addOrder'">Add Order</button>
+            <h6 class="bg-success"><?php display_message(); ?></h6>
+            <table class="table table-hover" role="table">
+                <thead role="rowgroup">
+                <tr role="row">
+                    <th scope="col" role="columnheader">Order ID</th>
+                    <th scope="col" role="columnheader">Order date</th>
+                    <th scope="col" role="columnheader">Order by</th>
+                    <th scope="col" role="columnheader">Email</th>
+                    <th scope="col" role="columnheader"></th>
+                </tr>
+                </thead>
+                <tbody role="rowgroup">
+                <div class="orderlist">
 
-    <!-- List of orders -->
-    <table class="table table-hover" role="table">
-      <thead role="rowgroup">
-      <tr role="row">
-          <th scope="col" role="columnheader">Ordered By</th>
-          <th scope="col" role="columnheader">Ordered On</th>
-          <th scope="col" role="columnheader">Order ID</th>
-          <th scope="col" role="columnheader"></th>
-      </tr>
-      </thead>
-      <tbody id="orders">
-    
-      </tbody>
-  </table>
-   <input type="submit" name="add_user" class="btn btn-dark pull-right" value="Save All" >
-</div>
+                </div>
+                </tbody>
+            </table>
 </div>
