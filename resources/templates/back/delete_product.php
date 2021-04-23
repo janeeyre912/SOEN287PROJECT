5db5acbedee08d3726dc3a4d98a22d5011b37e61
Michael <osujim2012@gmail.com>
2021-04-23 12:43:01 -0400

    fixed

--- resources/templates/back/delete_product.php
+++ resources/templates/back/delete_product.php
@@ -6,11 +6,11 @@ if(isset($_GET['delete_product_id'])){
   foreach ($xml->product as $theProduct){
     if($replace){
         $dom = dom_import_simplexml($theProduct);
+        $dom->itemNb(0)->nodeValue--;
     }  
     else if($theProduct->id == $_GET['delete_product_id']){
         $dom = dom_import_simplexml($theProduct);
         $dom->parentNode->removeChild($dom);
-        $dom->itemNb(0)->nodeValue--;
       }
   }
   file_put_contents('../datas/products.xml',$xml->asXML());
