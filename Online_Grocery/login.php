cf15d06e046dfc77a8730ddda2ef712735a64b2e
janeeyre912 <57924747+janeeyre912@users.noreply.github.com>
2021-04-23 19:35:47 -0400

    Update login.php

--- Online_Grocery/login.php
+++ Online_Grocery/login.php
@@ -1,7 +1,7 @@
 <?php require_once("../resources/config.php"); ?>
 <?php $title = "Login" ?>
 <?php include(TEMPLATE_FRONT . DS . "header.php"); ?>
-<h3 class="text-center bg-warning"><?php display_message(); ?></h3>
+
  <?php login_user(); ?>
 <div class="row justify-content-center">
   <div class="col-lg-8">
