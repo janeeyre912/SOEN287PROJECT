73e0d034cb8c96d3c32c438ecd36b3b5d91e1f8b
Atienn <70545238+Atienn@users.noreply.github.com>
2021-04-23 16:50:27 -0400

    Updated head & navbar links

--- Online_Grocery/shoppingCart/checkout.php
+++ Online_Grocery/shoppingCart/checkout.php
@@ -53,98 +53,49 @@
         </span>
       </div>
       <div class="logAccount col-lg-2">
-        <a class="login" href="../Online_Grocery/Auth/LoginPage.html">Login</a>
-        <a class="register" href="../Online_Grocery/Auth/SignUpPage.html"
-          >Register</a
-        >
+        <a class="login" href="../../Auth/LoginPage.html">Login</a>
+        <a class="register" href="../../Auth/SignUpPage.html">Register</a>
       </div>
     </div>
 
     <!--body-->
     <div class="navbarSticky">
-      <nav class="navbar navbar-expand-lg bg-c05555">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span>Navigation</span>
-        </button>
+  <nav class="navbar navbar-expand-lg bg-c05555">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span>Navigation</span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="../homePage.html"
-                >&nbsp HOME &nbsp<span class="sr-only">(current)</span></a
-              >
-            </li>
-            <a class="nav-link" href="../allProduct/all_products.html"
-              >&nbsp ALL PRODUCTS &nbsp</a
-            >
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                AISLES
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="../dairy_eggs/dairy_eggs.html"
-                  >Dairy & Eggs</a
-                >
-                <a
-                  class="dropdown-item"
-                  href="../Fruits&Vegetables/fruits_vegetables.html"
-                  >Fruits & Vegetables</a
-                >
-                <a class="dropdown-item" href="../seaFood/seafood.html"
-                  >Fish & Seafood</a
-                >
-                <a
-                  class="dropdown-item"
-                  href="../Meat&Poultry/MeatAndPoultry.html"
-                  >Meat & Poultry</a
-                >
-                <a class="dropdown-item" href="../Beverages/">Beverages</a>
-                <a class="dropdown-item" href="../Beer&Wine/beer_wine.html"
-                  >Beer & Wine</a
-                >
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">More...</a>
-              </div>
-            </li>
-            <a class="nav-link cart" href="../shoppingCart/shopping_cart.html"
-              ><i class="fas fa-shopping-cart"></i>&nbsp CART <span>0</span></a
-            >
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              id="search"
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              type="button"
-              class="btn btn-warning"
-              onclick="search_item()"
-            >
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="../">&nbsp HOME &nbsp<span class="sr-only">(current)</span></a>
+        </li>
+        <a class="nav-link" href="#">&nbsp ALL PRODUCTS &nbsp</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            AISLES
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" onclick="window.location.href='../basicPage.php?dairy_eggs'">Dairy & Eggs</a>
+            <a class="dropdown-item" onclick="window.location.href='../basicPage.php?fruits_vegetables'">Fruits & Vegetables</a>
+            <a class="dropdown-item" onclick="window.location.href='../basicPage.php?seafood'">Fish & Seafood</a>
+            <a class="dropdown-item" onclick="window.location.href='../basicPage.php?meat_poultry'">Meat & Poultry</a>
+            <a class="dropdown-item" onclick="window.location.href='../basicPage.php?beverages'">Beverages</a>
+            <a class="dropdown-item" onclick="window.location.href='../basicPage.php?beer_wine'">Beer & Wine</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">More...</a>
+          </div>
+        </li>
+        <a class="nav-link cart" href="shoppingCart/checkout.php"><i class="fas fa-shopping-cart"></i>&nbsp CART <span>0</span></a>
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input id="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button type="button" class="btn btn-warning" onclick="search_item()">Search</button>
+      </form>
     </div>
+  </nav>
+</div>
     <!--NavBarEND-->
 
     <div class="row justify-content-center">
