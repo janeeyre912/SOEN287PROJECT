ccc07afce71b2b000742b5e5c20f402c95a03957
Atienn <70545238+Atienn@users.noreply.github.com>
2021-04-23 18:57:58 -0400

    Updated page titles & footers

--- resources/templates/back/footer.php
+++ resources/templates/back/footer.php
@@ -1,7 +1,5 @@
-<!--footer-->
 <div class="footer">
-    Copyright &copy Fast groceries &nbsp; Technical support: 514 555 1234
+    Copyright &copy Fast groceries &nbsp; Technical support: (514) 555-1234
 </div>
-
 </body>
 </html>
