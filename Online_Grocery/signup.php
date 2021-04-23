eea505885507558326417d9ebdabb54a27991fbe
ThisIsQpac <christianjerjian@gmail.com>
2021-04-23 19:16:46 -0400

    header title

--- Online_Grocery/signup.php
+++ Online_Grocery/signup.php
@@ -1,6 +1,7 @@
 <?php require_once("../resources/config.php"); ?>
 <?php include(TEMPLATE_FRONT . DS . "header.php"); ?>
 <?php add_base(); ?>  
+<?php $title = "Sign Up" ?>
 
     <div class="row justify-content-center">
       <div class="col-lg-8">
--- resources/templates/front/header.php
+++ resources/templates/front/header.php
@@ -5,7 +5,15 @@
   <link rel="icon" href="img/favicon.png">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Fast Groceries</title>
+
+  
+  <title>
+    <?php if (isset($title)) {echo $title;}
+  else {echo "Fast Groceries";} ?>
+  </title>
+
+
+  <!-- <title>Homepage</title> -->
   <!--  CSS Stylesheets-->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="fastGroceries.css">
